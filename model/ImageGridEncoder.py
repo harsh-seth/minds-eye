251e5d501bfd62a09585d40f8e54037c4844d7b3
Harsh Seth <hseth@umass.edu>
2024-05-15 19:34:31 -0400

    Architecture and Scripts cloud ready

--- model/ImageGridEncoder.py
+++ model/ImageGridEncoder.py
@@ -1,52 +1,69 @@
 import torch
 import torch.nn as nn
-from transformers import AutoModel, AutoProcessor
 
 class ImageGridEncoder(nn.Module):
 
-    def __init__(self):
+    def __init__(self, in_channels=3, grid_n=3, embedding_dim=512, in_size=64, device='cuda'):
         super().__init__()
-        self.conv2d_1 = nn.Conv2d(in_channels=3, out_channels=10, kernel_size=5, padding=1, bias=True)
-        self.conv2d_2 = nn.Conv2d(in_channels=10, out_channels=20, kernel_size=5, padding=1, bias=True)
+        self.conv2d_1 = nn.Conv2d(in_channels=in_channels, out_channels=grid_n*grid_n, kernel_size=5, padding=2, bias=True)
+        self.conv2d_2 = nn.Conv2d(in_channels=grid_n*grid_n, out_channels=in_channels, kernel_size=5, padding=2, bias=True)
 
         self.relu = nn.ReLU()
+        self.flat = nn.Flatten()
 
-        self.maxpool_1 = nn.MaxPool2d(kernel_size=2)
 
         self.dropout_1 = nn.Dropout(0.3)
         self.dropout_2 = nn.Dropout(0.5)
         self.dropout_3 = nn.Dropout(0.2)
+        self.maxpool_1 = nn.MaxPool2d(kernel_size=2)
+        
+        mlp_ratio = 20
+        self.mlp1 = nn.Sequential(
+            nn.Linear(in_channels * (in_size//2)**2, embedding_dim*mlp_ratio),
+            nn.ReLU(),
+            nn.Linear(embedding_dim*mlp_ratio, embedding_dim)
+        )
 
-        self.flat = nn.Flatten()
+        self.norm1 = nn.LayerNorm(embedding_dim)
+        self.norm2 = nn.LayerNorm(embedding_dim)
+        self.norm3 = nn.LayerNorm(embedding_dim)
 
-        self.fc_1 = nn.Linear(20480, 10240)
-        self.fc_2 = nn.Linear(10240, 1024)
+        self.img_attn = nn.MultiheadAttention(embed_dim=embedding_dim, num_heads=4, batch_first=True)
+        self.pos_attn = nn.MultiheadAttention(embed_dim=embedding_dim, num_heads=4, batch_first=True)
 
-        self.dino_processor = AutoProcessor.from_pretrained("facebook/dinov2-base")
-        self.dino_model = AutoModel.from_pretrained("facebook/dinov2-base")
+        mlp_ratio = 2
+        self.mlp2 = nn.Sequential(
+            nn.Linear(embedding_dim, int(embedding_dim * mlp_ratio)),
+            nn.GELU(),
+            nn.Linear(int(embedding_dim * mlp_ratio), embedding_dim),
+        )
 
-        self.positional_embeddings = nn.Parameter(torch.randn((1024*768), 256))  # Learnable positional embeddings
-
-        self.vit_preprocessor = AutoProcessor.from_pretrained("google/vit-base-patch16-224")
-        self.vit_model = AutoModel.from_pretrained("google/vit-base-patch16-224")
-
+        self.positional_embeddings = nn.Parameter(torch.randn(embedding_dim)).unsqueeze(0).to(device)  # Learnable positional embeddings
 
     def forward(self, x):
-       # 3x64x64
+       # (N, 3, 64, 64)
+       # Convoluting a grid of KxK images to a stack of (KxK) "images"
        x = self.relu(self.conv2d_1(x))
        x = self.dropout_1(x)
-       # 10*64*64 to 20*64*64
+       # (N, 9, 64, 64)
+       # Convoluting a (KxK) stack of "images" to a "single image"
        x = self.relu(self.conv2d_2(x))
        x = self.dropout_2(x)
-       # 20*64*64 to 20*32*32
+       # (N, 3, 64, 64)
+       # Reducing "resolution" of the "single image", also lose "noise"/"empty values" from representation, ALSO help with ignoring redundant/repeated points!
        x = self.maxpool_1(x)
-       # 20*32*32 to 20480
+       # (N, 3, 32, 32)
+       # Flattening the "single image" into a vector to feed into a transformer
        x = self.flat(x)
-       x = self.relu(self.fc_1(x))
-       x = self.dropout_3(x)
-       x = self.fc_2(x)
-       x = self.dino_processor(x)
-       x = self.dino_model(x)
-       x = torch.cat((x, self.positional_embeddings), dim=1)
-       x = self.vit_model(**self.vit_preprocessor(x))
+       # (N, 3072)
+       x = self.mlp1(x)
+       # (N, 512)
+       # Encoding image features with self-attention
+       normed_x = self.norm1(x)
+       x = x + self.img_attn(normed_x, normed_x, normed_x, need_weights=False)[0]
+       x = x + self.mlp2(self.norm2(x))
+       # (N, 512)
+       # Combining image features with learnable position features with cross-attention (query: pos; key, val: img)
+       x = self.positional_embeddings + self.pos_attn(self.norm3(self.positional_embeddings), x, x, need_weights=False)[0]
+       # (N, 512)
        return x
--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 
 from data.Pix3dDataset import Pix3dDataset
 from model.MindsEye import MindsEye
-from utils.data_utils import save_checkpoint
+from utils.data_utils import make_dir_path, save_checkpoint
 
 def parse_command_line_arguments():
     parser = argparse.ArgumentParser(
@@ -16,12 +16,15 @@ def parse_command_line_arguments():
 
     parser.add_argument('--evaluate', '-e', action="store_true",
                         help='Only run evaluation')
+    
+    parser.add_argument('--triangles', action="store_true",
+                        help='Pass to run scripts for Triangles instead of Vertices')
 
     parser.add_argument('--batch_size', type=int, default=1,
                         help='mini-batch size (default: 1)')
 
-    parser.add_argument('--epochs', type=int, default=2,
-                        help='number of training epochs (default: 2)')
+    parser.add_argument('--epochs', type=int, default=100,
+                        help='number of training epochs (default: 100)')
 
     parser.add_argument('--lr', type=float, default=1e-4,
                         help='learning rate (AdamW) (default: 1e-4)')
@@ -29,16 +32,13 @@ def parse_command_line_arguments():
     parser.add_argument('--wd', type=float, default=0.001,
                         help='Weight Decay (AdamW) (default: 0.001)')
 
-    parser.add_argument('--workers', type=int, default=0,
-                        help='number of working units used to load the data (default: 0)')
-
     parser.add_argument('--device', default='cuda', type=str,
                         help='device to be used for computations (in {cpu, cuda:0, cuda:1, ...}, default: cuda)')
 
     parser.add_argument('--seed', type=int, default=42,
                         help='Seed for random initialization (default: 42)')
     
-    parser.add_argument('--resume_from_epoch', type=int, default=None,
+    parser.add_argument('--from_checkpoint', type=int, default=None,
                         help='Resume from checkpoint @ specified epoch number')
 
     parser.add_argument('--data_dir', type=str, default="./data/pix3d", 
@@ -47,17 +47,26 @@ def parse_command_line_arguments():
     parser.add_argument('--train_split', type=float, default=0.85,
                         help="Determines train/test split. Between [0.0, 1.0], default: 0.85")
     
+    parser.add_argument('--max_records', type=int, default=None,
+                        help="Determines the max number of records to a build dataset on, default: all records")
+
+    parser.add_argument('--max_vertices', type=int, default=4000,
+                    help="Determines the max number of vertices to allow in a single mesh, default: 4000")
+
+    parser.add_argument('--max_triangles', type=int, default=750,
+                    help="Determines the max number of triangles to allow in a single mesh, default: 750")
+    
     parsed_arguments = parser.parse_args()
     return parsed_arguments
 
-def evaluate(model, criterion, dataloader, device='cuda'):
+def evaluateOnce(model, criterion, dataloader, device='cuda', generate_triangles=False):
     model.to(device)
     model.eval()
     with torch.no_grad():
         total_loss = 0.0
         for batch in tqdm(dataloader):
             inputs = batch[0]
-            targets = batch[1][0]
+            targets = batch[1][int(generate_triangles)]
             inputs = inputs.to(device)
             targets = targets.to(device)
 
@@ -66,27 +75,31 @@ def evaluate(model, criterion, dataloader, device='cuda'):
             total_loss += loss.item()
     return total_loss/len(dataloader)
 
-def train(model, criterion, optimizer, train_dataloader, validation_dataloader, num_epochs, device='cuda', starting_epoch=0, save_path_prefix="results"):
+def trainOnce(model, criterion, optimizer, dataloader, device='cuda', generate_triangles=False):
     model.to(device)
+    model.train()
+    epoch_train_loss = 0.0
+    for batch in tqdm(dataloader):
+        optimizer.zero_grad()
+
+        inputs = batch[0]
+        targets = batch[1][int(generate_triangles)]
+        inputs = inputs.to(device)
+        targets = targets.to(device)
+
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+        epoch_train_loss += loss.item()
+
+        loss.backward()
+        optimizer.step()
+    return epoch_train_loss/len(dataloader)
+
+def train(model, criterion, optimizer, train_dataloader, validation_dataloader, num_epochs, device='cuda', starting_epoch=0, save_path_prefix="results", generate_triangles=False):
     for epoch in range(starting_epoch, num_epochs):
-        model.train()
-        epoch_train_loss = 0.0
-        for batch in tqdm(train_dataloader):
-            optimizer.zero_grad()
-
-            inputs = batch[0]
-            targets = batch[1][0]
-            inputs = inputs.to(device)
-            targets = targets.to(device)
-
-            outputs = model(inputs)
-            loss = criterion(outputs, targets)
-            epoch_train_loss += loss.item()
-
-            loss.backward()
-            optimizer.step()
         print(f"[Epoch {epoch + 1} / {num_epochs}]")
-        train_loss = epoch_train_loss/len(train_dataloader)
+
+        train_loss = trainOnce(model, criterion, optimizer, train_dataloader, device, generate_triangles=generate_triangles)
         print(f"\t Train Loss = {train_loss:.4f}")
 
         if (epoch+1) % 10 == 0:
@@ -95,13 +108,11 @@ def train(model, criterion, optimizer, train_dataloader, validation_dataloader, 
         with open(f"{save_path_prefix}/metrics.csv", "a") as results_file:
             results_file.write(f"train,{epoch+1},{train_loss:.4f}\n")
         
-        model.eval()
-        val_loss = evaluate(model, criterion, validation_dataloader, device)
+        val_loss = evaluateOnce(model, criterion, validation_dataloader, device, generate_triangles=generate_triangles)
         print(f"\t Val Loss = {val_loss:.4f}")
         with open(f"{save_path_prefix}/metrics.csv", "a") as results_file:
             results_file.write(f"val,{epoch+1},{val_loss:.4f}\n")
     
-    torch.save(model, f"{save_path_prefix}/checkpoints/checkpoint-{epoch+1}.pth")
     save_checkpoint(model, f"{save_path_prefix}/checkpoints/checkpoint-{epoch+1}.pth")
 
 if __name__ == '__main__':
@@ -110,20 +121,25 @@ if __name__ == '__main__':
         print(k + '=' + str(v))
     
     set_seed(args.seed)
-    save_path_prefix = "results"
+    save_path_prefix = "results/triangles" if args.triangles else "results/vertices"
+    make_dir_path(save_path_prefix)
     
-    model = torch.load(f'{save_path_prefix}/checkpoints/checkpoint-{args.resume_from_epoch}.pth', map_location=lambda storage, location: storage) if args.resume_from_epoch else MindsEye()
-    optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr, weight_decay=args.wd)
-    criterion = nn.L1Loss()
-
-    dataset = Pix3dDataset(data_dir=args.data_dir)
+    dataset = Pix3dDataset(data_dir=args.data_dir, max_records=args.max_records, max_vertices=args.max_vertices, max_triangles=args.max_triangles)
     train_dataset, validation_dataset = random_split(dataset, [args.train_split, 1.0 - args.train_split])
 
     train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
     validation_dataloader = DataLoader(validation_dataset, batch_size=args.batch_size, shuffle=True)
     print(f"Dataloaders ready. Train set length: {len(train_dataloader)}, Validation set length: {len(validation_dataloader)}")
 
-    if not args.evaluate:
+    model = torch.load(f'{save_path_prefix}/checkpoints/checkpoint-{args.from_checkpoint}.pth', map_location=lambda storage, location: storage) if args.from_checkpoint else MindsEye(generate_triangles=args.triangles, num_vertices=args.max_vertices, num_triangles=args.max_triangles)
+    criterion = nn.L1Loss()
+    if args.evaluate:
+        test_loss = evaluateOnce(model, criterion, validation_dataloader, args.device, generate_triangles=args.triangles)
+        print(f"\t Test Loss = {test_loss:.4f}")
+        with open(f"{save_path_prefix}/metrics.csv", "a") as results_file:
+            results_file.write(f"test,-1,{test_loss:.4f}\n")
+    else:
+        optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr, weight_decay=args.wd)
         train(
                 model=model,
                 criterion=criterion,
@@ -132,11 +148,7 @@ if __name__ == '__main__':
                 validation_dataloader=validation_dataloader,
                 num_epochs=args.epochs,
                 device=args.device,
-                starting_epoch=args.resume_from_epoch if args.resume_from_epoch else 0,
+                starting_epoch=args.from_checkpoint if args.from_checkpoint else 0,
                 save_path_prefix=save_path_prefix,
+                generate_triangles=args.triangles,
             )
-
-    test_loss = evaluate(model, criterion, validation_dataloader, args.device)
-    print(f"\t Test Loss = {test_loss:.4f}")
-    with open(f"{save_path_prefix}/metrics.csv", "a") as results_file:
-        results_file.write(f"test,-1,{test_loss:.4f}\n")
