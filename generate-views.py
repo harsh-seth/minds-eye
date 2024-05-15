d822c53be80d222a84d3d74b5e892c84059c8ae9
Dhrumeen Patel <dhrumeen.dp@gmail.com>
2024-05-15 01:50:01 -0400

    Add conv, Dino and Transformer network code

--- generate-views.py
+++ generate-views.py
@@ -1,3 +1,56 @@
 # Given a 3D model, create an image of the object at a specified rotation and translation (distance) from the object
 def modelToView(model, rotation=0.0, translation=0.0):
   pass
+
+
+import torch
+import torch.nn as nn
+import torch.optim as optimizer
+import torchvision
+from transformers import AutoModel, AutoProcessor
+
+class ConvDino(nn.Module):
+
+    def __init__(self):
+        super.__init__()
+        self.conv2d_1 = nn.Conv2d(in_channels=3, out_channels=10, kernel_size=5, padding=1, bias=True)
+        self.conv2d_2 = nn.Conv2d(in_channels=10, out_channels=20, kernel_size=5, padding=1, bias=True)
+
+        self.relu = nn.ReLU()
+
+        self.maxpool_1 = nn.MaxPool2d(kernel_size=2)
+
+        self.dropout_1 = nn.Dropout(0.3)
+        self.dropout_2 = nn.Dropout(0.5)
+        self.dropout_3 = nn.Dropout(0.2)
+
+        self.fc_1 = nn.Linear(1800000, 1800)
+        self.fc_2 = nn.Linear(1800, 1024)
+
+        self.dino_processor = AutoProcessor.from_pretrained("facebook/dinov2-base")
+        self.dino_model = AutoModel.from_pretrained("facebook/dinov2-base")
+
+        self.positional_embeddings = nn.Parameter(torch.randn((1024*768), 256))  # Learnable positional embeddings
+
+        self.vit_preprocessor = AutoProcessor.from_pretrained("google/vit-base-patch16-224")
+        self.vit_model = AutoModel.from_pretrained("google/vit-base-patch16-224")
+
+
+    def forward(self, x):
+       # 3x600x600
+       x = self.relu(self.conv2d_1(x))
+       x = self.dropout_1(x)
+       # 10*600*600 to 20*600*600
+       x = self.relu(self.conv2d_2(x))
+       x = self.dropout_2(x)
+       # 20*600*600 to 20*300*300
+       x = self.maxpool_1(x)
+       # 20*300*300 to 1800000
+       x = self.relu(self.fc_1(x))
+       x = self.dropout_3(x)
+       x = self.fc_2(x)
+       x = self.dino_processor(x)
+       x = self.dino_model(x)
+       x = torch.cat((x, self.positional_embeddings), dim=1)
+       x = self.vit_model(**self.vit_preprocessor(x))
+       return x
